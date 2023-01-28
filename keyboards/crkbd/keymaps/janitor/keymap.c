ae065c669c2cce4a56d7ec47d990560a0106a1c5
Max Fiialkovskyi <maxfiialkovskyi@protonmail.com>
2023-01-28 16:45:13 +0100

    tab the keymap for cosmetics

--- keyboards/crkbd/keymaps/janitor/keymap.c
+++ keyboards/crkbd/keymaps/janitor/keymap.c
@@ -52,76 +52,49 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BASE] = LAYOUT_split_3x6_3(
-  //,------------------     ------------  ---------      ---------       -----.                    ,--------------------         ---------       ----------      ------          --------.
-       KC_BTN2,   KC_Q,      MEH_T(KC_W), HYPR_T(KC_E),      KC_R,           KC_T,                         KC_Y,    KC_U,       HYPR_T(KC_I),MEH_T(KC_O),         KC_P,             KC_BTN2,
-  //|--------+--------+     --------+---  -----+---      -----+---       -----|                    |--------+--------+--         ------+--       ------+---      -----+          --------|
-      KC_BTN3, LGUI_T(KC_A), LALT_T(KC_S),LCTL_T(KC_D), LSFT_T(KC_F),      KC_G,                         KC_H, LSFT_T(KC_J), LCTL_T(KC_K),  LALT_T(KC_L), LGUI_T (KC_SCLN),         KC_QUOT,
-  //|--------+--------+---   -----+-     -------  +     --------+-       - ------|                    |--------+--------+------  --+------      --+--------     +--------|
-      KC_BTN1,    KC_Z,       KC_X,         KC_C,         KC_V,            KC_B,                         KC_N,    KC_M,             KC_COMM,        KC_DOT,       KC_SLSH,          KC_BTN1,
-  //|--------+--------+---   -----+-     -------+--     ------+---                                -----+--------|  |--------+--  ------+--      ------+----     ----+--------+--------+--------|
-                                                  KC_ESC, LT(NAV, KC_SPC),  KC_TAB,                      LT(NAV, KC_ENT),   LT (NUM, KC_BSPC), LT(FUN, KC_DELETE)
-                                              //`--------------------------'  `                         --------------------------'
+    KC_BTN2 , KC_Q         , MEH_T(KC_W)  , HYPR_T(KC_E) , KC_R         , KC_T ,            KC_Y , KC_U         , HYPR_T(KC_I) , MEH_T(KC_O)  , KC_P             , KC_BTN2 ,
+    KC_BTN3 , LGUI_T(KC_A) , LALT_T(KC_S) , LCTL_T(KC_D) , LSFT_T(KC_F) , KC_G ,            KC_H , LSFT_T(KC_J) , LCTL_T(KC_K) , LALT_T(KC_L) , LGUI_T (KC_SCLN) , KC_QUOT ,
+    KC_BTN1 , KC_Z         , KC_X         , KC_C         , KC_V         , KC_B ,            KC_N , KC_M         , KC_COMM      , KC_DOT       , KC_SLSH          , KC_BTN1 ,
 
+                                                KC_ESC, LT(NAV, KC_SPC), KC_TAB,            LT(NAV, KC_ENT),   LT (NUM, KC_BSPC), LT(FUN, KC_DELETE)
   ),
 
-    // TODO: mod taps on symbol layer
   [NUM] = LAYOUT_split_3x6_3(
-  //,---------      ----------       -------        ---------       ---------      --------.                    ,-----------------------------------------------------.
-      XXXXXXX,          KC_1,    MEH_T(KC_2),   HYPR_T(KC_3),    KC_4,             KC_5,                         KC_6,    KC_7,             HYPR_T(KC_8),    MEH_T(KC_9),       KC_0,           XXXXXXX,
-  //|--------+      --------+-       ------+        --------+       --------+      --------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX, LGUI_T(KC_EXLM),  LALT_T(KC_AT), LCTL_T(KC_HASH), LSFT_T(KC_DLR),   KC_PERC,                      KC_CIRC, LSFT_T(KC_AMPR),  LCTL_T(KC_ASTR), LALT_T(DW_PLUS),   LGUI(DW_EQUAL), XXXXXXX,
-  //|--------+      --------+-       ------+        --------+       --------+      --------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX, TD(TD_PARENS),    TD(TD_BRACES), TD(TD_BRACKETS), TD(TD_SLASHES),   TD(TD_GRAVE),                 KC_PIPE, DW_QUES,          XXXXXXX,         XXXXXXX,           XXXXXXX,        XXXXXXX,
-  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                                XXXXXXX,         XXXXXXX,          XXXXXXX,                      XXXXXXX, KC_TRNS,          XXXXXXX
-                                      //`--------------------------'  `--------------------------'
+    XXXXXXX , KC_1            , MEH_T(KC_2)   , HYPR_T(KC_3)    , KC_4           , KC_5         ,                   KC_6    , KC_7            , HYPR_T(KC_8)    , MEH_T(KC_9)     , KC_0           , XXXXXXX ,
+    XXXXXXX , LGUI_T(KC_EXLM) , LALT_T(KC_AT) , LCTL_T(KC_HASH) , LSFT_T(KC_DLR) , KC_PERC      ,                   KC_CIRC , LSFT_T(KC_AMPR) , LCTL_T(KC_ASTR) , LALT_T(DW_PLUS) , LGUI(DW_EQUAL) , XXXXXXX ,
+    XXXXXXX , TD(TD_PARENS)   , TD(TD_BRACES) , TD(TD_BRACKETS) , TD(TD_SLASHES) , TD(TD_GRAVE) ,                   KC_PIPE , DW_QUES         , XXXXXXX         , XXXXXXX         , XXXXXXX        , XXXXXXX ,
 
+                                                XXXXXXX , XXXXXXX, XXXXXXX,             XXXXXXX, KC_TRNS, XXXXXXX
   ),
 
   [NAV] = LAYOUT_split_3x6_3(
-      // TODO: Add function keys to nav (or somewhere)
-  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     DF(GAME),  KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, XXXXXXX,
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_INS , KC_HOME,  KC_PGDN, KC_PGUP, KC_END, XXXXXXX,
-  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          XXXXXXX, KC_TRNS, XXXXXXX,    KC_TRNS , XXXXXXX, KC_TRNS
-                                      //`--------------------------'  `--------------------------'
+    XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,                 XXXXXXX  , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX  , XXXXXXX ,
+    XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,                 DF(GAME) , KC_LEFT , KC_DOWN , KC_UP   , KC_RIGHT , XXXXXXX ,
+    XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,                 KC_INS   , KC_HOME , KC_PGDN , KC_PGUP , KC_END   , XXXXXXX ,
+
+                                     XXXXXXX, KC_TRNS, XXXXXXX,                 KC_TRNS , XXXXXXX, KC_TRNS
   ),
 
   [FUN] = LAYOUT_split_3x6_3(
-  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      XXXXXXX,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                        KC_F6,   KC_F7 ,   KC_F8,  KC_F9,  KC_F10, XXXXXXX,
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       KC_F11,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       KC_F12, XXXXXXX,  XXXXXXX, XXXXXXX,XXXXXXX, XXXXXXX,
-  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          XXXXXXX, KC_TRNS, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX
-                                      //`--------------------------'  `--------------------------'
+    XXXXXXX,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,             KC_F6,      KC_F7 ,  KC_F8,   KC_F9,   KC_F10,  XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,             KC_F11,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,             KC_F12,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+
+                               XXXXXXX, KC_TRNS, XXXXXXX,             XXXXXXX, XXXXXXX, XXXXXXX
   ),
+
   [GAME] = LAYOUT_split_3x6_3(
-  //,-----------------------------------------------------.                           ,-----------------------------------------------------.
-      KC_ESC,  KC_TAB,     KC_Q,    KC_W,    KC_E,    KC_R,                             XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  //|--------+--------+--------+--------+--------+--------|                           |--------+--------+--------+--------+--------+--------|
-      MO(GAMEFUN), KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,                            DF(BASE), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  //|--------+--------+--------+--------+--------+--------|                           |--------+--------+--------+--------+--------+--------|
-      KC_DEL, KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,                             XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  //|--------+--------+--------+--------+--------+--------+--------|         |--------+--------+--------+--------+--------+--------+--------|
-                                           KC_SPC,  KC_BTN2,  KC_BTN1,    KC_ESC, XXXXXXX, XXXXXXX
-                                      //`--------------------------'         `--------------------------'
+    KC_ESC,       KC_TAB,     KC_Q,    KC_W,    KC_E,     KC_R,       XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    MO(GAMEFUN),  KC_LCTL,    KC_A,    KC_S,    KC_D,     KC_F,       DF(BASE), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    KC_DEL,       KC_LSFT,    KC_Z,    KC_X,    KC_C,     KC_V,       XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+
+                                    KC_SPC,  KC_BTN2,  KC_BTN1,       KC_ESC, XXXXXXX, XXXXXXX
   ),
   [GAMEFUN] = LAYOUT_split_3x6_3(
-  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      XXXXXXX,    KC_1,    KC_2, KC_TRNS,    KC_3,    KC_4,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX, KC_ESC,     KC_M,    KC_T,    KC_G,    KC_B,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX
-                                      //`--------------------------'  `--------------------------'
+    XXXXXXX , KC_1   , KC_2  , KC_TRNS , KC_3  , KC_4  ,                     XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,
+    XXXXXXX , KC_F1  , KC_F2 , KC_F3   , KC_F4 , KC_F5 ,                     XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,
+    XXXXXXX , KC_ESC , KC_M  , KC_T    , KC_G  , KC_B  ,                     XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,
+
+                               XXXXXXX, XXXXXXX, XXXXXXX,                    XXXXXXX, XXXXXXX, XXXXXXX
   )
 };
