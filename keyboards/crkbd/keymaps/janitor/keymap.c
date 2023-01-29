40d4fcb5ad14d14a1af0533aa8b184c73a2c51e0
Max Fiialkovskyi <maxfiialkovskyi@protonmail.com>
2023-01-29 20:35:07 +0100

    add qmk boot keys to base

--- keyboards/crkbd/keymaps/janitor/keymap.c
+++ keyboards/crkbd/keymaps/janitor/keymap.c
@@ -65,17 +65,17 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BASE] = LAYOUT_split_3x6_3(
-    KC_BTN2 , KC_Q         , MEH_T(KC_W)  , HYPR_T(KC_E) , KC_R         , KC_T ,            KC_Y , KC_U         , HYPR_T(KC_I) , MEH_T(KC_O)  , KC_P             , KC_BTN2 ,
-    KC_BTN3 , LGUI_T(KC_A) , LALT_T(KC_S) , LCTL_T(KC_D) , LSFT_T(KC_F) , KC_G ,            KC_H , LSFT_T(KC_J) , LCTL_T(KC_K) , LALT_T(KC_L) , LGUI_T (KC_SCLN) , KC_QUOT ,
+    QK_MAKE , KC_Q         , MEH_T(KC_W)  , HYPR_T(KC_E) , KC_R         , KC_T ,            KC_Y , KC_U         , HYPR_T(KC_I) , MEH_T(KC_O)  , KC_P             , KC_BTN2 ,
+    QK_BOOT, LGUI_T(KC_A) , LALT_T(KC_S) , LCTL_T(KC_D) , LSFT_T(KC_F) , KC_G ,            KC_H , LSFT_T(KC_J) , LCTL_T(KC_K) , LALT_T(KC_L) , LGUI_T (KC_SCLN) , KC_QUOT ,
     KC_BTN1 , KC_Z         , KC_X         , KC_C         , KC_V         , KC_B ,            KC_N , KC_M         , KC_COMM      , KC_DOT       , KC_SLSH          , KC_BTN1 ,
 
-                                                KC_ESC, LT(NAV, KC_SPC), KC_TAB,            KC_ENT,   LT (NUM, KC_BSPC), LT(OSL(FUN), KC_DELETE)
+                                                KC_ESC, LT(NAV, KC_SPC), KC_TAB,            KC_ENT,   LT (NUM, KC_BSPC), LT(FUN, KC_DELETE)
   ),
 
   [NUM] = LAYOUT_split_3x6_3(
-    XXXXXXX , KC_1            , MEH_T(KC_2)   , HYPR_T(KC_3)    , KC_4           , KC_5         ,                   KC_6    , KC_7            , HYPR_T(KC_8)    , MEH_T(KC_9)     , KC_0           , XXXXXXX ,
-    XXXXXXX , LGUI_T(KC_EXLM) , LALT_T(KC_AT) , LCTL_T(KC_HASH) , LSFT_T(KC_DLR) , KC_PERC      ,                   KC_CIRC , LSFT_T(KC_AMPR) , LCTL_T(KC_ASTR) , LALT_T(DW_PLUS) , LGUI(DW_EQUAL) , XXXXXXX ,
-    XXXXXXX , TD(TD_PARENS)   , TD(TD_BRACES) , TD(TD_BRACKETS) , TD(TD_SLASHES) , TD(TD_GRAVE) ,                   KC_PIPE , DW_QUES         , XXXXXXX         , XXXXXXX         , XXXXXXX        , XXXXXXX ,
+    XXXXXXX , KC_1            , MEH_T(KC_2)   , HYPR_T(KC_3)    , KC_4           , KC_5         , KC_6    , KC_7            , HYPR_T(KC_8)    , MEH_T(KC_9)     , KC_0           , XXXXXXX ,
+    XXXXXXX , LGUI_T(KC_EXLM) , LALT_T(KC_AT) , LCTL_T(KC_HASH) , LSFT_T(KC_DLR) , KC_PERC      , KC_CIRC , LSFT_T(KC_AMPR) , LCTL_T(KC_ASTR) , LALT_T(DW_PLUS) , LGUI(DW_EQUAL) , XXXXXXX ,
+    XXXXXXX , TD(TD_PARENS)   , TD(TD_BRACES) , TD(TD_BRACKETS) , TD(TD_SLASHES) , TD(TD_GRAVE) , KC_PIPE , DW_QUES         , XXXXXXX         , XXXXXXX         , XXXXXXX        , XXXXXXX ,
 
                                                 XXXXXXX , REPEAT, XXXXXXX,             XXXXXXX, KC_TRNS, XXXXXXX
   ),
