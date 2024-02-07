c608ad6477136f6c6d9016f800f5f818c512d9ae
Max Fiialkovskyi <maxfiialkovskyi@protonmail.com>
2024-02-07 14:56:18 +0100

    stop space from firing too much, add meh mod tap

--- keyboards/crkbd/keymaps/janitor/keymap.c
+++ keyboards/crkbd/keymaps/janitor/keymap.c
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_BTN3 , KC_A         , KC_S         , KC_D         , KC_F         , KC_G ,            KC_H , KC_J         , KC_K         , KC_L         , KC_SCLN          , KC_BTN3 ,
     KC_BTN1 , KC_Z         , KC_X         , KC_C         , KC_V         , KC_B ,            KC_N , KC_M         , KC_COMM      , KC_DOT       , KC_SLSH          , KC_BTN1 ,
 
-    KC_ESC, KC_SPC, OSM(MOD_LSFT),        OSM(MOD_LCTL), TO(1), XXXXXXX
+    KC_ESC, MT(MOD_MEH, KC_SPC), OSM(MOD_LSFT),        OSM(MOD_LCTL), TO(1), XXXXXXX
   ),
 
   [1] = LAYOUT_split_3x6_3(
