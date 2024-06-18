4f91e1198b2b3e2796914d7d6510e1a4c75db0cb
Max Fiialkovskyi <maxfiialkovskyi@protonmail.com>
2024-06-18 17:48:12 +0200

    the answer is 34

--- keyboards/bespoke1/keymaps/j/keymap.c
+++ keyboards/bespoke1/keymaps/j/keymap.c
@@ -9,45 +9,42 @@
 #define DW_SLSH KC_LEFT_BRACKET
 
 
-enum Layers { BASE, NUM, NAV, MOUSE, FUN };
+enum Layers { BASE, NUM, CTL, NAV, FUN };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BASE] = LAYOUT_split_3x5__3(
-    KC_Q         , MEH_T(KC_W)  , HYPR_T(KC_E) , KC_R         , KC_T ,            KC_Y , KC_U         , HYPR_T(KC_I) , MEH_T(KC_O)  , KC_P             ,
-    LGUI_T(KC_A) , LALT_T(KC_S) , LCTL_T(KC_D) , LSFT_T(KC_F) , KC_G ,            KC_H , LSFT_T(KC_J) , LCTL_T(KC_K) , LALT_T(KC_L) , LGUI_T (KC_SCLN) ,
-    KC_Z         , KC_X         , KC_C         , KC_V         , KC_B ,            KC_N , KC_M         , KC_COMM      , KC_DOT       , KC_SLSH          ,
-                                      LM(NUM, MOD_LSFT), LT(NAV, KC_SPC), OSM(MOD_LSFT),        OSM(MOD_LCTL), QK_REP,    OSL(NUM)
+    KC_Q         , KC_W         , KC_E         , KC_R         , KC_T , KC_Y , KC_U         , KC_I         , KC_O         , KC_P             ,
+    LGUI_T(KC_A) , LALT_T(KC_S) , LCTL_T(KC_D) , LSFT_T(KC_F) , KC_G , KC_H , LSFT_T(KC_J) , LCTL_T(KC_K) , LALT_T(KC_L) , LGUI_T (KC_SCLN) ,
+    KC_Z         , KC_X         , KC_C         , KC_V         , KC_B , KC_N , KC_M         , KC_COMM      , KC_DOT       , KC_SLSH          ,
+
+                                      LT(CTL, KC_SPC), TT(NAV), XXXXXXX,XXXXXXX,           LM(NUM,MOD_LSFT ),OSL(NUM)
   ),
-
-
   [NUM] = LAYOUT_split_3x5__3(
     _______ , _______ , _______ , _______ , XXXXXXX , XXXXXXX , KC_BSLS , KC_GRV  , DW_LBRC , DW_RBRC ,
     KC_1    , KC_2    , KC_3    , KC_4    , KC_5    , KC_6    , KC_7    , KC_8    , KC_9    , KC_0    ,
     XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , DW_MINS , DW_SLSH , DW_EQL  , XXXXXXX ,
 
-                                                XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX
+                                                TO(BASE),XXXXXXX  , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX
+  ),
+  [CTL] = LAYOUT_split_3x5__3(
+    XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , KC_ESC  , KC_BSPC , KC_DEL  ,
+    XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , KC_ENT  , KC_TAB  , XXXXXXX , XXXXXXX ,
+    XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , KC_HOME , KC_PGDN , KC_PGUP , KC_END  ,
+
+                         TO(BASE), XXXXXXX       , XXXXXXX       , XXXXXXX        , XXXXXXX  , MO(FUN)
   ),
   [NAV] = LAYOUT_split_3x5__3(
-    XXXXXXX       , OSM(MOD_MEH)  , OSM(MOD_HYPR) , XXXXXXX       , XXXXXXX , TG(MOUSE) , XXXXXXX , KC_ESC  , KC_BSPC  , KC_DEL  ,
-    OSM(MOD_LGUI) , OSM(MOD_LALT) , OSM(MOD_LCTL) , OSM(MOD_LSFT) , XXXXXXX , KC_LEFT   , KC_DOWN , KC_UP   , KC_RIGHT , XXXXXXX ,
-    XXXXXXX       , XXXXXXX       , XXXXXXX       , XXXXXXX       , XXXXXXX , XXXXXXX   , KC_HOME , KC_PGDN , KC_PGUP  , KC_END  ,
+    XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , C(KC_I)   , KC_BTN1 , KC_BTN2 , KC_BTN3 , XXXXXXX  ,
+    XXXXXXX , KC_MS_L , KC_MS_D , KC_MS_U , KC_MS_R , XXXXXXX   , KC_LEFT , KC_DOWN , KC_UP   , KC_RIGHT ,
+    XXXXXXX , KC_WH_L , KC_WH_D , KC_WH_U , KC_WH_R , C(KC_DOT) , KC_HOME , KC_PGDN , KC_PGUP , KC_END   ,
 
-                         XXXXXXX , XXXXXXX       , XXXXXXX       , KC_ENT        , KC_TAB  , MO(FUN)
+                         TO(BASE), XXXXXXX       , XXXXXXX       , XXXXXXX        , XXXXXXX  , MO(FUN)
   ),
-
-  [MOUSE] = LAYOUT_split_3x5__3(
-    XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , TG(MOUSE) , C(KC_I) , XXXXXXX , XXXXXXX , XXXXXXX ,
-    XXXXXXX , KC_MS_L , KC_MS_D , KC_MS_U , KC_MS_R , XXXXXXX , KC_BTN1 , KC_BTN2 , KC_BTN3 , XXXXXXX ,
-    XXXXXXX , KC_WH_L , KC_WH_D , KC_WH_U , KC_WH_R , XXXXXXX , XXXXXXX , XXXXXXX , C(KC_DOT) , XXXXXXX ,
-
-                         XXXXXXX , XXXXXXX       , XXXXXXX       , XXXXXXX, XXXXXXX, XXXXXXX
-  ),
-
   [FUN] = LAYOUT_split_3x5__3(
-    KC_F1   , MEH_T(KC_F2) , HYPR(KC_F3) , KC_F4   , KC_F5   ,            KC_F6  , KC_F7   , HYPR_T(KC_F8) , MEH_T(KC_F9) , KC_F10  ,
-    XXXXXXX , XXXXXXX      , XXXXXXX     , XXXXXXX , XXXXXXX ,            KC_F11 , XXXXXXX , XXXXXXX       , XXXXXXX      , XXXXXXX ,
-    XXXXXXX , XXXXXXX      , XXXXXXX     , XXXXXXX , XXXXXXX ,            KC_F12 , XXXXXXX , XXXXXXX       , XXXXXXX      , XXXXXXX ,
+    KC_F1   , KC_F2   , KC_F3   , KC_F4   , KC_F5   , KC_F6  , KC_F7   , KC_F8   , KC_F9   , KC_F10  ,
+    XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , KC_F11 , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,
+    XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , KC_F12 , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,
 
-                                  XXXXXXX, XXXXXXX, XXXXXXX,              XXXXXXX, XXXXXXX, XXXXXXX
+                                  TO(BASE), XXXXXXX, XXXXXXX,              XXXXXXX, XXXXXXX, XXXXXXX
   ),
 };
